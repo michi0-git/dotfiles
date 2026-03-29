79dab3278879dd26fdde4a602cbad680d62bc6ed
michi0-git <209760957+michi0-git@users.noreply.github.com>
2026-03-29 16:53:17 +0700

    update: iterm2 and nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -26,6 +26,38 @@ require("lazy").setup({
   { 'nvim-tree/nvim-tree.lua', lazy = false, dependencies = { 'nvim-tree/nvim-web-devicons' }, config = function() require("nvim-tree").setup {} end },
   { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' }, config = function() require('lualine').setup { options = { theme = 'dracula' } } end },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = {
+            ensure_installed = { "lua", "python", "javascript", "typescript", "vim", "vimdoc", "bash" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "pyright" }, 
+        })
+        -- ここで各LSPを有効化する設定を書く（後述）
+    end,
+  },
 })
 
 vim.cmd.colorscheme "catppuccin"
