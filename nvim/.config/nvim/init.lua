6f4c61eb8f422c74c953ccbeb5cfc9a840cf6ea7
michi0-git <209760957+michi0-git@users.noreply.github.com>
2026-04-01 21:01:57 +0700

    update: nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,6 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
-
 vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -22,7 +21,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } },
   { 'nvim-tree/nvim-tree.lua', lazy = false, dependencies = { 'nvim-tree/nvim-web-devicons' }, config = function() require("nvim-tree").setup {} end },
   { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' }, config = function() require('lualine').setup { options = { theme = 'dracula' } } end },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
@@ -55,7 +53,20 @@ require("lazy").setup({
         require("mason-lspconfig").setup({
             ensure_installed = { "lua_ls", "pyright" }, 
         })
-        -- ここで各LSPを有効化する設定を書く（後述）
+
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({})
+        lspconfig.pyright.setup({
+            settings = {
+                python = {
+                    analysis = {
+                        autoSearchPaths = true,
+                        useLibraryCodeForTypes = true,
+                        diagnosticMode = "workspace",
+                    },
+                },
+            },
+        })
     end,
   },
 })
